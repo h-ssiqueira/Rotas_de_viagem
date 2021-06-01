77d8cf518d1f6c7214df063de855964a87bd8291
Henrique <hss@outlook.com.br>
2021-05-31 22:10:15 -0300

    Comentários com os requisitos do projeto
    O que o programa deve fazer / estrutura de cada caso de teste

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Nós representam as cidades
+// Arestas representam as rodovias
+// Matriz de adjacência
+
+// Fazer 20 casos de teste em um .txt
+/*
+* Cada um com
+* taxa de proporção do lucro desejado P
+* nº de cidades a ser considerada N (5<=N<=20)
+* nº de ligações rodoviárias E (0<=E<=9900)
+* taxa de lucro esperada T (1<=T<=100)
+* matriz adjacente que representa a ligação rodoviária direta entre duas cidades e contem as despesas e receitas desta ligação
+*/
+
+// O programa deve imprimir um relatório com o nº de caso se existe rota cíclica, quantas rotas cíclicas existem e qual a relação do lucro T e do valor P desejado. Imprimir o resultado em outro .txt
+
+
+// Resumindo:
+
+/*
+* Coleta do arquivo os casos de teste
+* Manipular os dados dos casos de teste para encontrar ciclo e encontrar o(s) ciclo(s) de menor custo
+* Imprimir o relatório com o nº de casos, se existe rota cíclica e seu respectivo nº e a relação do lucro e valor P considerado
+*/
+
+int main(){
+
+	return 0;
+}
