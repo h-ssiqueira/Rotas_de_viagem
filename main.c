032f5e166273c0d3f910e8d5450816c866aab556
Henrique <hss@outlook.com.br>
2021-06-07 15:29:16 -0300

    Correção de erros
    Correção de erros de compilação e testes de execução.
    
    Tratamento do arquivo de saída, caso exista (execução anterior) o programa o zera automaticamente.
    
    Criação de uma função de exibição da matriz de adjacência.

--- main.c
+++ main.c
@@ -29,13 +29,14 @@
 #define MAX_CIDADES 20
 
 // Desaloca as listas internas e zera a quantidade de caminhos de cada vértice da matriz
-void zera_matriz(float **MA, int tam){
+void zera_matriz(float MA[][20], int tam){
 	for(int i = 0; i < tam; i++){
 		for(int j = 0; j < tam; j++){
 			MA[i][j] = -1.0;
 		}
 	}
 }
+// REVER
 //solução que calcule o ciclo com maior proporção de lucro (Receitas totais / Despesas totais)
 /*float calcula_prop(int receita, int despesa){
 	float maior,des;
@@ -51,16 +52,16 @@ void zera_matriz(float **MA, int tam){
 }*/
 
 // Exibe os resultados e grava em um arquivo txt
-void resultados(int ciclos, float proporcaoP, float lucroP, int n_caso){
+void resultados(int ciclos, float P, int n_caso){
 	FILE *resultado;
 	int res;
-	printf("\n\nCaso número %d\n%2d Rotas cíclicas\nProporção de lucro %.2f\n\n", n_caso,ciclos, lucroP / proporcaoP);
+	printf("\n\nCaso número %d\n%2d Rotas cíclicas\nProporção de lucro %.2f\n\n", n_caso,ciclos, P);
 	resultado = fopen("resultados.txt","a");
 	if(!resultado){
 		printf("Falha ao abrir/criar o arquivo");
 		exit(1);
 	}
-	res = fprintf(resultado,"Caso número %d\n%2d Rotas cíclicas\nProporção de lucro %.2f\n\n\n", n_caso,ciclos, lucroP / proporcaoP);
+	res = fprintf(resultado,"Caso número %d\n%2d Rotas cíclicas\nProporção de lucro %.2f\n\n\n", n_caso,ciclos, P);
 	if(res == EOF){
 		printf("\n\nErro na gravação dos dados no arquivo\n");
 		exit(1);
@@ -68,7 +69,23 @@ void resultados(int ciclos, float proporcaoP, float lucroP, int n_caso){
 	fclose(resultado);
 }
 
-void encontra_ciclo(float **MA, int tam){
+// Exibe a matriz de adjacência para testes
+void print_matriz(float MA[][20], int tam){
+	printf("\n\n     ");
+	for(int i = 0; i < tam; i++)
+		printf("  %2d  ", i+1);
+	printf("\n");
+	for(int i = 0; i < tam; i++){
+		printf("%2d   ", i+1);
+		for(int j = 0; j < tam; j++)
+			printf("%5.2f ", MA[i][j]);
+		printf("\n");
+	}
+	printf("\n\n");
+}
+
+// Pode retornar se houve ciclo / custo do ciclo / vetor com os vértices do ciclo
+void encontra_ciclo(float MA[][20], int tam){
 	// BFS
 	// Floyd's tortoise and hare
 }
@@ -79,19 +96,25 @@ int main(int argc, char **argv){
 
 	float MA[MAX_CIDADES][MAX_CIDADES];
 
-	//Alocar matriz
-	zera_matriz(MA,20);
-
 
 	//Leitura do arquivo de casos de teste
 	FILE *casos_de_teste;
 	int n_caso,proporcaoP,n_cidades,n_caminhos;
 	int orig,dest,lucro,desp;
+	// Variáveis para manipulação do grafo
+	int n_ciclos;
+	float proporcaoG;
 
-	if(argc != 1){
+
+	if(argc != 2){
 		printf("Parametros incorretos");
 		exit(-1);
 	}
+
+	// Limpa os resultados da execução anterior se existirem
+	casos_de_teste = fopen("resultados.txt","w");
+	if(casos_de_teste)
+		fclose(casos_de_teste);
 
 	casos_de_teste = fopen(argv[1],"r");
 	if(!casos_de_teste){
@@ -106,17 +129,31 @@ int main(int argc, char **argv){
 		//Pega a quantidade de cidades (vertices)
 		// Pega a quantidade de caminhos (arestas) entre as cidades (vertices)
 
+		// Prepara a matriz para alocar o grafo
+		zera_matriz(MA,20);
 
-
-		// Aloca lista de arestas
+		// Aloca as posições na matriz
 
 		for(int c=0;c<n_caminhos;c++){
 			fscanf(casos_de_teste,"%d %d %d %d\n",&orig,&dest,&lucro,&desp); // Pra cada caminho(aresta) pega: origem,destino,lucro, despesa
-			//MA[orig][dest].num_arestas+=1; //Incrementa quantidade de caminhos
-			//insere_aresta(MA[orig][dest].conexoes,lucro,desp); //Inserir aresta na lista da MA
+			if(MA[orig][dest] < (float) lucro / desp)
+				MA[orig][dest] = (float) lucro / desp;
 		}
 
+		// Para testes
+		print_matriz(MA,n_cidades);
+
+		n_ciclos = 0;
+		proporcaoG = 0.0;
+
+		// Algoritmo de busca de ciclo (loop entre os vértices? Pode retornar se encontrou ciclo (problema de duplicidade) ou o lucro do ciclo)
+		encontra_ciclo(MA,n_cidades);
 
 
+
+		resultados(n_ciclos, proporcaoG / proporcaoP, n_caso);
+	}
+
+	fclose(casos_de_teste);
 	return 0;
 }
